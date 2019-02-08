72299a69ee29052b9f3034ab96ebb055f66ce163
Cory <corybishop5@gmail.com>
2019-02-08 11:28:48 -0500

    4th 500 after success 1st 2nd, 3rd only 400

--- cloud/app.js
+++ cloud/app.js
@@ -229,12 +229,12 @@ Parse.Cloud.job("Migrate_Activity", function(request, status) {
 
 var actQuery = Parse.Object.extend("Activity");
 var query = new Parse.Query(actQuery);
-query.skip(1000);
+query.skip(1400);
 query.limit(500);
     query.find({
             success:function(results) {
 
-            console.info("total old activity needing to 3rd 500  = "+results.length);
+            console.info("total old activity needing to 4th skip 1400 500  = "+results.length);
                 for (var i = 0; i < results.length; i++) {
                        var result = results[i];
 
@@ -315,7 +315,7 @@ query.limit(500);
                 }
               }
 
-                      status.success("Activity Migration 3rd 500 successfull");
+                      status.success("Activity Migration 4th 500 successfull");
             },
             error: function(error) {
             status.error("Uh oh, something went wrong.");
