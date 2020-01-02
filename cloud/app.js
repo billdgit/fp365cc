8239f9b395ada4ebd55a45c943bd6cdbc249e72f
Cory <corybishop5@gmail.com>
2020-01-02 05:07:45 -0500

    skip 1800

--- cloud/app.js
+++ cloud/app.js
@@ -229,7 +229,7 @@ var actQuery = Parse.Object.extend("Activity");
 var query = new Parse.Query(actQuery);
 
 query.limit(300);
-query.skip(1500);
+query.skip(1800);
 
     query.find({
             success:function(results) {
