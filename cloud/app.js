2ed225ac0fef6e4d23e4cef8c575233e603d2607
Cory <corybishop5@gmail.com>
2020-01-02 04:18:00 -0500

    2nd 300

--- cloud/app.js
+++ cloud/app.js
@@ -229,7 +229,7 @@ var actQuery = Parse.Object.extend("Activity");
 var query = new Parse.Query(actQuery);
 
 query.limit(300);
-//query.skip(506);
+query.skip(300);
 
     query.find({
             success:function(results) {
